b2f7adceb17ace8e93237278c100c4ab7eeeda79
dhruvilkotak <kotakdhruvil@gmail.com>
2025-06-23 17:38:18 -0700

    Update webclient end point

--- src/main/java/com/monitoring/RCAService.java
+++ src/main/java/com/monitoring/RCAService.java
@@ -7,7 +7,7 @@ import reactor.core.publisher.Mono;
 @Service
 public class RCAService {
 
-    private final WebClient client = WebClient.create("http://localhost:8000");
+    private final WebClient client = WebClient.create("https://ai-rca-service.onrender.com/rca");
 
     public Mono<RCAResponse> callRCA(String log) {
         RCARequest request = new RCARequest(log);
